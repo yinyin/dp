cbcb764c8656e57b872b246173a239d97aea9dda
Yinyin Liao <yinyinl@gmail.com>
2012-07-10 00:54:41 +0800

    add stroy content to Story object

--- lib/dpcore.py
+++ lib/dpcore.py
@@ -1,9 +1,45 @@
 
 # -*- coding: utf-8 -*-
 
+import hashlib
+import base64
+
+
+_every_story = {}
+
+def __build_object_id(sig, attempt, prefix):
+	""" create id for given object signature
+	"""
+	
+	h = hashlib.md5(sig+str(attempt))
+	b64digest = base64.b64encode(h.digest(), "ZY")
+	return prefix + b64digest[0:22]
+# ### def __build_object_id	
+
+def _allocate_story_id(storyobj):
+	""" allocate a unique story id for given story object
+	"""
+	
+	if (storyobj.story_id is not None) or (storyobj.story is None):
+		return storyobj.story_id
+	
+	sig = str(storyobj)
+	attempt = 0
+	idcandidate = __build_object_id(sig, attempt, "C")
+	while idcandidate in _every_story:
+		attempt = attempt + 1
+		idcandidate = __build_object_id(sig, attempt, "C")
+	
+	_every_story[idcandidate] = storyobj
+	
+	return idcandidate
+# ### def _allocate_story_id
+	
+	
+		
 
 class Story(object):
-	def __init__(self, story_id, story, note, imp_order, imp_value, point, demo_method):
+	def __init__(self, story_id, story, note, imp_order, imp_value, point, demo_method, sort_order_key=None):
 		self.story_id = story_id
 		self.story = story
 		self.note = note
@@ -11,7 +47,24 @@ class Story(object):
 		self.imp_value = imp_value
 		self.point = point
 		self.demo_method = demo_method
+		
+		self.sort_order_key = sort_order_key
+		
+		self.__prepare_story_id()
 	# ### def __init__
+	
+	def __prepare_story_id(self):
+		""" generate story-id if it is not present
+		"""
+		
+		if self.story_id is None:
+			self.story_id = _allocate_story_id(self)
+	# ### def __prepare_story_id
+	
+	def __repr__(self):
+		return "%s.Story(story_id=%r, story=%r, note=%r, imp_order=%r, imp_value=%r, point=%r, demo_method=%r, sort_order_key=%r)" % (self.__module__, self.story_id, self.story, self.note, self.imp_order, self.imp_value, self.point, self.demo_method, self.sort_order_key,)
+	# ### def __repr__
+	
 # ### class Story
 
 
@@ -25,6 +78,19 @@ def _convert_to_string(v):
 	
 	return v
 # ### def _convert_to_string
+
+def _convert_to_integer(v):
+	try:
+		v = unicode(v).strip()
+		if 0 == len(v):
+			v = None
+		else:
+			v = int(v)
+	except Exception as e:
+		v = None
+	
+	return v
+# ### def _convert_to_integer
 
 def load_stories(m):
 	""" load stories from m
@@ -47,13 +113,32 @@ def load_stories(m):
 		point = None
 		demo_method = None
 		
+		sort_order_key = None
+		
 		if "story-id" in m:
 			story_id = str(m["story-id"])
 		if "story" in m:
 			story = _convert_to_string(m["story"])
 		if "note" in m:
 			note = _convert_to_string(m["note"])
-		# TODO
+		if "order" in m:
+			imp_order = _convert_to_string(m["order"])
+			if imp_order is not None:
+				v = imp_order.upper()
+				if "HIGH" == v:
+					sort_order_key = 9
+				elif "NORMAL" == v:
+					sort_order_key = 5
+				elif "LOG" == v:
+					sort_order_key = 1
+		if "value" in m:
+			imp_value = _convert_to_string(m["value"])
+		if "point" in m:
+			point = _convert_to_integer(m["point"])
+		if "demo-method" in m:
+			demo_method = _convert_to_string(m["demo-method"])
 		
+		obj = Story(story_id, story, note, imp_order, imp_value, point, demo_method, sort_order_key)
+		result = (obj,)
 	return result
 # ### def load_stories
