980b6d521da5a43e6d5429d36cf3a771353ae165
Yinyin Liao <yinyinl@gmail.com>
2012-07-20 00:43:55 +0800

    add test code of task and log

---
+++ test/test_log.py
@@ -0,0 +1,72 @@
+
+# -*- coding: utf-8 -*-
+
+import unittest
+
+import testing_common
+
+import dpcore
+
+
+class TestLogLoad(unittest.TestCase):
+	""" test load_logs() function """
+
+	def test_load_logs_1(self):
+		""" load log with string object """
+
+		m = "This is a log"
+		loglist = dpcore.load_logs(m)
+
+		self.assertEqual(m, loglist[0].log)
+	# ### def test_load_tasks_1
+
+	def test_load_logs_2(self):
+		""" load log with dict object (with task definition attached) """
+
+		m = {"l": "This is a log"}
+		loglist = dpcore.load_logs(m)
+
+		self.assertEqual(m["l"], loglist[0].log)
+	# ### def test_load_logs_2
+
+	def test_load_logs_4(self):
+		""" load task with dict object (without any content) """
+
+		m = {}
+		loglist = dpcore.load_logs(m)
+
+		self.assertEqual(0, len(loglist))
+	# ### def test_load_logs_4
+
+	def test_load_logs_5(self):
+		""" load log with list object """
+
+		m = ["this is log 1", "this is log 2",]
+		loglist = dpcore.load_logs(m)
+
+		self.assertEqual(2, len(loglist))
+		idx = 0
+		for mm in m:
+			self.assertEqual(mm, loglist[idx].log)
+			idx = idx + 1
+	# ### def test_load_logs_5
+
+	def test_load_logs_6(self):
+		""" load log with dict object (basic information) """
+
+		m = {"l": "a log of development.", "record-time": "2012-07-20 03:12:59",
+				"author": "Test User"}
+		loglist = dpcore.load_logs(m)
+
+		self.assertEqual(m["l"], loglist[0].log)
+		self.assertEqual(datetime.datetime(2012, 7, 20, 3, 12, 59), loglist[0].record_time)
+		self.assertEqual(m["author"], loglist[0].author)
+	# ### def test_load_logs_6
+# ### class TestLogLoad
+
+
+
+if __name__ == '__main__':
+	unittest.main()
+
+# vim: ts=4 sw=4 ai nowarp
---
+++ test/test_task.py
@@ -0,0 +1,85 @@
+
+# -*- coding: utf-8 -*-
+
+import unittest
+
+import testing_common
+
+import dpcore
+
+
+class TestTaskLoad(unittest.TestCase):
+	""" test load_tasks() function """
+
+	def test_load_tasks_1(self):
+		""" load task with string object """
+
+		m = "This is a task"
+		tasklist = dpcore.load_tasks(m)
+
+		self.assertEqual(m, tasklist[0].task)
+	# ### def test_load_tasks_1
+
+	def test_load_tasks_2(self):
+		""" load task with dict object (with task definition attached) """
+
+		m = {"t": "This is a task"}
+		tasklist = dpcore.load_tasks(m)
+
+		self.assertEqual(m["t"], tasklist[0].task)
+	# ### def test_load_tasks_2
+
+	def test_load_tasks_3(self):
+		""" load task with dict object (without task definition attached, but attached valid key-value pair) """
+
+		m = {"point": 6}
+		tasklist = dpcore.load_tasks(m)
+
+		self.assertEqual(None, tasklist[0].task)
+	# ### def test_load_tasks_3
+
+	def test_load_tasks_4(self):
+		""" load task with dict object (without any valid key-value pair) """
+
+		m = {}
+		tasklist = dpcore.load_tasks(m)
+
+		self.assertEqual(0, len(tasklist))
+	# ### def test_load_tasks_4
+
+	def test_load_tasks_5(self):
+		""" load task with list object """
+
+		m = ["this is task 1", "this is task 2",]
+		tasklist = dpcore.load_tasks(m)
+
+		self.assertEqual(2, len(tasklist))
+		idx = 0
+		for mm in m:
+			self.assertEqual(mm, tasklist[idx].task)
+			idx = idx + 1
+	# ### def test_load_tasks_5
+
+	def test_load_tasks_6(self):
+		""" load story with dict object (basic information) """
+
+		m = {"t": "a task of development.", "note": "* multiple line\n* notes and notes",
+				"point": 7, "estimated-time": 3, "status": "new", "test-method": "use demo system to demo."}
+		tasklist = dpcore.load_tasks(m)
+
+		self.assertEqual(m["t"], tasklist[0].task)
+		self.assertEqual(m["note"], tasklist[0].note)
+		self.assertEqual(7, tasklist[0].point)
+		self.assertEqual(m["estimated-time"], tasklist[0].estimated_time)
+		self.assertEqual(None, tasklist[0].status)
+		self.assertEqual(m["test-method"], tasklist[0].test_method)
+		self.assertTrue(tasklist[0].task_id is not None)
+	# ### def test_load_tasks_6
+# ### class TestTaskLoad
+
+
+
+if __name__ == '__main__':
+	unittest.main()
+
+# vim: ts=4 sw=4 ai nowarp
