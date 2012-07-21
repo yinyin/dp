add8339265d995adc5abb08c059b6b71b2809407
Yinyin Liao <yinyinl@gmail.com>
2012-07-21 17:00:07 +0800

    refactor: extract allocate_object_id from allocate_story_id

--- lib/dpcore.py
+++ lib/dpcore.py
@@ -5,43 +5,79 @@ import hashlib
 import base64
 
 
-_every_story = {}
+
+class IdentifiableObject(object):
+	""" defined an interface for objects which can have an identify"""
+
+	def __init__(self, *args, **kwargs):
+		super(IdentifiableObject, self).__init__(*args, **kwargs)
+	# ### def __init__
+
+	def get_object_id(self):
+		return None
+	# ### def get_object_id
+
+	def set_object_id(self, new_id):
+		pass
+	# ### def get_object_id
+
+	def get_object_signature(self):
+		return None
+	# ### def get_object_signature
+# ### class IdentifiableObject
 
 def __build_object_id(sig, attempt, prefix):
 	""" create id for given object signature
+
+	Argument:
+		sig - object signature/content
+		attempt - number of attempts
+		prefix - prefix of identity
+	Return:
+		candidate identity
 	"""
-	
+
 	h = hashlib.md5(sig+str(attempt))
 	b64digest = base64.b64encode(h.digest(), "ZY")
 	return prefix + b64digest[0:22]
-# ### def __build_object_id	
+# ### def __build_object_id
 
-def _allocate_story_id(storyobj):
-	""" allocate a unique story id for given story object
+def allocate_object_id(idableobj, idprefix, objectrepo):
+	""" allocate a unique id for given identifiable object
+
+	Argument:
+		idableobj - an IdentifiableObject object
+		idprefix - prefix of identity
+		objectrepo - object repository (a dict which has identity as key and object as value)
+	Return:
+		allocated identity
 	"""
-	
-	if (storyobj.story_id is not None) or (storyobj.story is None):
-		return storyobj.story_id
-	
-	sig = str(storyobj)
+
+	idableobj_id = idableobj.get_object_id()
+	idableobj_sig = idableobj.get_object_signature()
+	if (idableobj_id is not None) or (idableobj_sig is None):
+		return idableobj_id
+
 	attempt = 0
-	idcandidate = __build_object_id(sig, attempt, "C")
-	while idcandidate in _every_story:
+	idcandidate = __build_object_id(idableobj_sig, attempt, idprefix)
+	while idcandidate in objectrepo:
 		attempt = attempt + 1
-		idcandidate = __build_object_id(sig, attempt, "C")
-	
-	_every_story[idcandidate] = storyobj
-	
+		idcandidate = __build_object_id(idableobj_sig, attempt, idprefix)
+
+	objectrepo[idcandidate] = idableobj
+	idableobj.set_object_id(idcandidate)
+
 	return idcandidate
-# ### def _allocate_story_id
-	
-	
-		
+# ### def _allocate_object_id
+
+
+
+_every_story = {}
 
 class Story(object):
 	def __init__(self, story_id, story, note, imp_order, imp_value, point, demo_method, sort_order_key=None, *args, **kwargs):
 		super(Story, self).__init__(*args, **kwargs)
-		
+
 		self.story_id = story_id
 		self.story = story
 		self.note = note
@@ -49,31 +85,31 @@ class Story(object):
 		self.imp_value = imp_value
 		self.point = point
 		self.demo_method = demo_method
-		
+
 		self.sort_order_key = sort_order_key
-		
+
 		self.__prepare_story_id()
 	# ### def __init__
-	
+
 	def __prepare_story_id(self):
 		""" generate story-id if it is not present
 		"""
-		
+
 		if self.story_id is None:
 			self.story_id = _allocate_story_id(self)
 	# ### def __prepare_story_id
-	
+
 	def __repr__(self):
 		return "%s.Story(story_id=%r, story=%r, note=%r, imp_order=%r, imp_value=%r, point=%r, demo_method=%r, sort_order_key=%r)" % (self.__module__, self.story_id, self.story, self.note, self.imp_order, self.imp_value, self.point, self.demo_method, self.sort_order_key,)
 	# ### def __repr__
-	
+
 # ### class Story
 
 
 class Task(object):
 	def __init__(self, task_id, task, note, estimated_time, status, test_method, *args, **kwargs):
 		super(Task, self).__init__(*args, **kwargs)
-		
+
 		pass
 	# ### def __init__
 # ### class Task
@@ -86,7 +122,7 @@ def _convert_to_string(v):
 			v = None
 	except Exception as e:
 		v = None
-	
+
 	return v
 # ### def _convert_to_string
 
@@ -99,16 +135,16 @@ def _convert_to_integer(v):
 			v = int(v)
 	except Exception as e:
 		v = None
-	
+
 	return v
 # ### def _convert_to_integer
 
 def load_stories(m):
 	""" load stories from m
 	"""
-	
+
 	result = []
-	
+
 	if isinstance(m, (list, tuple,)):
 		for mm in m:
 			result.extend(load_stories(mm))
@@ -124,11 +160,11 @@ def load_stories(m):
 		imp_value = None
 		point = None
 		demo_method = None
-		
+
 		sort_order_key = None
-		
+
 		is_accepted_any_attribute = False
-		
+
 		if "story-id" in m:
 			story_id = str(m["story-id"])
 			is_accepted_any_attribute = True
@@ -158,7 +194,7 @@ def load_stories(m):
 		if "demo-method" in m:
 			demo_method = _convert_to_string(m["demo-method"])
 			is_accepted_any_attribute = True
-		
+
 		if is_accepted_any_attribute:
 			obj = Story(story_id, story, note, imp_order, imp_value, point, demo_method, sort_order_key)
 			result = (obj,)
